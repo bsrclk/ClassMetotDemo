f311e4937be6ff382299134709f3cc630a896b3f
ÇOLAK <OLAK@DESKTOP-8FPMOJU>
2021-07-27 11:18:48 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Bilgileri ekleme=" +musteri.MusteriAdi+musteri.MusteriSoyadi+musteri.MusteriTel);
+        }
+
+        public void Listeleme(Musteri musteri)
+        {
+           Console.WriteLine( "Musteri Bilgileri Listeleme=" + musteri.MusteriAdi + musteri.MusteriSoyadi + musteri.MusteriTel);
+        }
+
+
+        public void silme(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Bilgileri silme=" + musteri.MusteriAdi + musteri.MusteriSoyadi + musteri.MusteriTel);
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriAdi = "ada";
+            musteri1.MusteriSoyadi = "çolak";
+            musteri1.MusteriTel = 054155696;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriAdi = "arda";
+            musteri2.MusteriSoyadi = "çolak";
+            musteri2.MusteriTel = 0541585696;
+
+            Musteri[] musteri = new Musteri[] { musteri1, musteri2 };
+
+            foreach (Musteri musteriler in musteri)
+            {
+                Console.WriteLine(musteriler.MusteriAdi);
+                Console.WriteLine(musteriler.MusteriSoyadi);
+                Console.WriteLine(musteriler.MusteriTel);
+
+            }
+
+            Console.WriteLine("******************************");
+
+            MusteriManager musteriManager = new MusteriManager();
+
+           
+            
+            musteriManager.Ekle(musteri1);
+            musteriManager.Listeleme(musteri);
+            musteriManager.silme(musteri2);
+           
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
